7b2c1044ffdaf31abcca8b8c305e3d7ac4143375
bilalloumghari <bilalloumghari@icloud.com>
2025-04-12 14:07:22 +0200

    Added first test image

---
+++ test_image.py
@@ -0,0 +1,4 @@
+from diffusers import StableDiffusionPipeline
+pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+image = pipe("professional portrait").images[0]
+image.save("test.png")
